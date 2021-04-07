0790ccdeb074158d0ce476bce1ce646eedc2e568
EC2 Default User <ec2-user@ip-172-31-79-235.ec2.internal>
2021-04-07 07:44:19 +0000

    [finish]2-7

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,3 @@
-class List <
+class List < ApplicationRecord
   attachment :image
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b147e51938ee87eb798ab6a74433086ef7e1bc1183b613d6b8e24e79d539551d9cbd47687ffac047b0b80c6e3262b16b34a77a74e5fc768e1bbe868143c08a57'
